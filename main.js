b32843525ff0c7ff94122432f72ed30a14d2abca
rodrigomramos21 <rodrigomramos18@hotmail.com>
2023-01-27 16:10:51 -0300

    teste

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function setTeamName(team1Name, team2Name) {
   document.getElementById("team1").getElementsByClassName("name")[0].innerHTML = team1Name;
   document.getElementById("team2").getElementsByClassName("name")[0].innerHTML = team2Name;
 }
-setTeamName("Time 1", "Time 2")
+setTeamName("Time 1", "Time 2") 
 
 
 
